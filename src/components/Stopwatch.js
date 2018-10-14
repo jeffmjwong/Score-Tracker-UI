9bba292dfbb54d54be2ba1c33bed8fd35e3cbb82
Jeff Wong <jeffmjwong@gmail.com>
2018-10-14 20:16:28 +1000

    Complete stopwatch logic function

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,24 +2,45 @@ import React, { Component } from 'react';
 
 class Stopwatch extends Component {
   state = {
-    isRunning: false
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
   };
+
+  componentDidMount() {
+    this.intervalID = setInterval(this.tick, 100);
+  }
+
+  tick = () => {
+    const { isRunning } = this.state;
+
+    if (isRunning) {
+      this.setState((prevState) => ({
+        elapsedTime: prevState.elapsedTime + (Date.now() - prevState.previousTime),
+        previousTime: Date.now()
+      }));
+    }
+  }
 
   handleStopwatch = () => {
     this.setState((prevState) => ({
       isRunning: !prevState.isRunning
-    }));
+    }), () => {
+      const { isRunning } = this.state;
+      if (isRunning) this.setState({ previousTime: Date.now() });
+    });
   }
 
   render() {
-    const { isRunning } = this.state;
+    const { isRunning, elapsedTime } = this.state;
+    const seconds = Math.floor(elapsedTime / 1000);
 
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">0</span>
+        <span className="stopwatch-time">{ seconds }</span>
         <button onClick={this.handleStopwatch}>{ isRunning ? 'Stop' : 'Start' }</button>
-        <button>Reset</button>
+        <button onClick={() => this.setState({ elapsedTime: 0 })}>Reset</button>
       </div>
     );
   }
